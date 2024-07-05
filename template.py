15cff0db93664162ff2f10aae7cac1b170e86eca
AayushSalvi <salviaayush30@gmail.com>
2024-07-05 13:25:55 +0530

    template creation

--- template.py
+++ template.py
@@ -18,6 +18,15 @@ list_of_files = [
    "pyproject.toml",
    "tox.ini",
    "experiments/experiments.ipynb", 
+   "src/components/data_ingestion.py",
+   "src/components/data_transformation.py",
+   "src/components/model_trainer.py",
+   "src/components/model_evaluation.py",
+   "src/pipeline/__init__.py",
+   "src/pipeline/training_pipeline.py",
+   "src/pipeline/prediction_pipeline.py",
+   "src/utils/utils.py",
+
 ]
 
 for filepath in list_of_files:
