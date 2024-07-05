ca782e56b9306e7f8496f363c220a2f0a724dee6
AayushSalvi <salviaayush30@gmail.com>
2024-07-05 13:32:01 +0530

    template creation

--- template.py
+++ template.py
@@ -26,6 +26,9 @@ list_of_files = [
    "src/pipeline/training_pipeline.py",
    "src/pipeline/prediction_pipeline.py",
    "src/utils/utils.py",
+   "src/logger/__init__.py",
+   "src/logger/logging.py",
+   "src/exception/exception.py"
 
 ]
 
